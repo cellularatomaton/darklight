3f950127b33006ed23de027144539e70789017fb
walter moore <walter.moore@CHB1S07R1LT.FCStone.com>
2012-09-25 10:08:20 -0500

    Caliburn based MVVM

---
+++ DarkLight/ViewModels/TestViewModel.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using Caliburn.Micro;
+
+namespace DarkLight.ViewModels
+{
+    public class TestViewModel : PropertyChangedBase
+    {
+        string name;
+
+        public string Name
+        {
+            get { return name; }
+            set
+            {
+                name = value;
+                NotifyOfPropertyChange(() => Name);
+                NotifyOfPropertyChange(() => CanSayHello);
+            }
+        }
+
+        private string message;
+        public string Message
+        {
+            get { return message; }
+            set 
+            { 
+                message = value;
+                NotifyOfPropertyChange(() => Message);
+            }
+        }
+
+        public bool CanSayHello
+        {
+            get { return !string.IsNullOrWhiteSpace(Name); }
+        }
+
+        public void SayHello()
+        {
+            Message = "Hello " + Name + "!";
+        }
+    }
+}
