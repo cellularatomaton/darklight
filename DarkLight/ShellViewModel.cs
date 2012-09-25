5e721c1aa8b546391f6f96c8bda2a0f4abe0b454
walter moore <walter.moore@CHB1S07R1LT.FCStone.com>
2012-09-25 10:08:20 -0500

    Caliburn based MVVM

---
+++ DarkLight/ShellViewModel.cs
@@ -0,0 +1,26 @@
+﻿using Caliburn.Micro;
+using DarkLight.ViewModels;
+using System.ComponentModel.Composition;
+
+namespace DarkLight 
+{
+    [Export(typeof(IShell))]
+    public class ShellViewModel : PropertyChangedBase, IShell
+    {
+        private TestViewModel testViewModel;
+        public TestViewModel TestViewModel
+        {
+            get { return testViewModel; }
+            set 
+            { 
+                testViewModel = value;
+                NotifyOfPropertyChange(() => TestViewModel);
+            }
+        }
+
+        public ShellViewModel()
+        {
+            TestViewModel = new TestViewModel();
+        }
+    }
+}
